83b37d0a9d5d71ef20936e421a492b5950a34e98
James Johnson <music1384@yahoo.co.uk>
2017-07-25 15:21:23 +0100

    Created and added playercontroller.

---
+++ Assets/Code/Classes/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Rigidbody2D), typeof(Collider2D))]
+public class PlayerController : MonoBehaviour
+{
+    [Tooltip("The speed of the player's left and right movement.")]
+    [SerializeField] private float _Speed = 5f;
+
+    /// The player's direction with speed.
+    private Vector3 _Velocity = Vector3.zero;
+    /// Reference to the player's Rigibody component.
+    private Rigidbody2D _Rigidbody2D = null;
+    /// Reference to the player's Transform component.
+    private Transform _Transform = null;
+
+    private void Awake ()
+    {
+        AssignReferences ();
+    }
+
+    private void AssignReferences ()
+    {
+        _Rigidbody2D = GetComponent<Rigidbody2D> ();
+        _Transform = GetComponent<Transform> ();
+        GetComponent<Collider2D> ().isTrigger = false;
+    }
+
+    private void Start ()
+    {
+        SetDefaults ();
+    }
+
+    private void SetDefaults ()
+    {
+        _Rigidbody2D.gravityScale = 0.0f;
+        _Rigidbody2D.isKinematic = false;
+    }
+
+    private void Update ()
+    {
+        GetInput ();
+    }
+
+    private void GetInput ()
+    {
+        var dir = new Vector3 (Input.GetAxis ("Horizontal"), 0.0f, 0.0f);
+
+        _Velocity = dir * _Speed;
+    }
+
+    private void FixedUpdate ()
+    {
+        Move ();
+    }
+
+    private void Move ()
+    {
+        _Rigidbody2D.velocity = _Velocity * Time.fixedDeltaTime;
+    }
+
+    private void Die ()
+    {
+        Destroy (this.gameObject);
+    }
+}
