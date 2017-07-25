c00edc570f915441093d3e756e6e73f3242c05db
James Johnson <music1384@yahoo.co.uk>
2017-07-25 16:39:31 +0100

    Created and added obstacle class

---
+++ Assets/Code/Classes/Obstacle.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Rigidbody2D), typeof(Collider2D))]
+public class Obstacle : MonoBehaviour
+{
+    [Tooltip("The speed at which the obstacle moves down the screen.")]
+    [SerializeField] private float _Speed = 0.0f;
+    [Tooltip("The distance before the object is culled from play.")]
+    [SerializeField] private float _CullingRange = 0.0f;
+
+    /// Reference to the object's rigidbody component.
+    private Rigidbody2D _Rigidbody2D = null;
+    /// Reference to the objects transform component.
+    private Transform _Transform = null;
+
+    private void Awake ()
+    {
+        AssignReferences ();
+    }
+
+    private void AssignReferences ()
+    {
+        _Rigidbody2D = GetComponent<Rigidbody2D> ();
+        _Transform = GetComponent<Transform> ();
+
+        GetComponent<Collider2D> ().isTrigger = false;
+    }
+
+    private void Start ()
+    {
+        SetDefaults ();
+    }
+
+    private void SetDefaults ()
+    {
+        _Rigidbody2D.gravityScale = 0.0f;
+        _Rigidbody2D.isKinematic = false;
+    }
+
+    private void Update ()
+    {
+        CheckPosition ();
+    }
+
+    private void CheckPosition ()
+    {
+        if (_Transform.position <= _CullingRange)
+            Cull ();
+    }
+
+    private void FixedUpdate ()
+    {
+        Move ();
+    }
+
+    private void Move ()
+    {
+        _Rigidbody2D.velocity = Vector2.down * _Speed * Time.fixedDeltaTime;
+    }
+
+    private void Cull ()
+    {
+        Destroy (this.gameObject);
+    }
+}
