e6cf0028e6b33452e63101c80fa084d22bee024f
James Johnson <music1384@yahoo.co.uk>
2017-07-25 16:52:39 +0100

    Created basic gamecontroller class

---
+++ Assets/Code/Classes/GameController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    /// The total score of the player in the game so far.
+    public int Score { get { return _Score; } set { ChangeScore (value); } }
+
+    [Tooltip("The player's total score in the game so far.")]
+    [SerializeField] private int _Score = 0;
+
+    private void ChangeScore (int score)
+    {
+        _Score = score;
+    }
+}
